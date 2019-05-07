90c08efbca155e582e31d68d56ae6d21367e59c4
Hilding Linden <hilding.linden@abo.fi>
2019-05-07 15:08:37 +0300

    Add lidar parameters

--- ai_parser.py
+++ ai_parser.py
@@ -50,9 +50,14 @@ class Monitor(object):
 
     bp2 = world.get_blueprint_library().find('sensor.lidar.ray_cast')
     bp2.set_attribute('range', '1000')
-    bp2.set_attribute('rotation_frequency', '360')
+    bp2.set_attribute('points_per_second', '1000')
+    bp2.set_attribute('channels', '9')
+    bp2.set_attribute('upper_fov', '0')
+    bp2.set_attribute('lower_fov', '-45')
+    bp2.set_attribute('sensor_tick', '0.5')
+    #bp2.set_attribute('rotation_frequency', '360')
     self.lidar = world.spawn_actor(bp2, carla.Transform(), attach_to=self.vehicle)
-    #self.lidar.set_transform(carla.Transform(carla.Location(z=3)))
+    self.lidar.set_transform(carla.Transform(carla.Location(z=2)))
     self.lidar.listen(lambda event: Monitor._lidar_update(weak_self, event))
 
 
@@ -97,10 +102,10 @@ class Analyser(object):
 
     if not lidar == 0:     
       for location in lidar:
-          print location
-          #rel_loc = carla.Location(car_loc.x+location.x, car_loc.y+location.y, car_loc.z+location.z)
-          #self.knowledge.retrieve_data('world').debug.draw_point(rel_loc,
-          #                              color=carla.Color(r=255, g=0, b=0), life_time=1.0)
+          #print location
+          rel_loc = carla.Location(car_loc.x+location.x, car_loc.y+location.y, car_loc.z+location.z+2)
+          self.knowledge.retrieve_data('world').debug.draw_point(rel_loc,
+                                        color=carla.Color(r=255, g=0, b=0), life_time=1.0)
 
     self.knowledge.update_data('max_speed', 5)
 
